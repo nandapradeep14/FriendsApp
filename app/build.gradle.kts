e6bbc551f7a28bb958579fdc858c3832d72390ae
nandapradeep14 <nandapradeeppayeri@gmail.com>
2024-06-29 15:27:04 +0530

    project updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
 
+    implementation("com.android.volley:volley:1.2.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/nanda/friendsapp/MainActivity.java
+++ app/src/main/java/com/nanda/friendsapp/MainActivity.java
@@ -1,24 +1,101 @@
 package com.nanda.friendsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.jar.Attributes;
+
 public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2,ed3,ed4;
+    AppCompatButton b1;
+    String apiurl="https://friendsapi-re5a.onrender.com/adddata";
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ed1=(EditText) findViewById(R.id.name);
+        ed2=(EditText) findViewById(R.id.fname);
+        ed3=(EditText) findViewById(R.id.fnick);
+        ed4=(EditText) findViewById(R.id.des);
+        b1=(AppCompatButton) findViewById(R.id.sub);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getName=ed1.getText().toString();
+                String getFname=ed2.getText().toString();
+                String getFnickname=ed3.getText().toString();
+                String getDesc=ed4.getText().toString();
+                Toast.makeText(getApplicationContext(),getName+getFname+getFnickname+getDesc,Toast.LENGTH_LONG).show();
+                JSONObject friend=new JSONObject();
+                try {
+                    friend.put("name", getName);
+                    friend.put("friendName",getFname);
+                    friend.put("friendNickName",getFnickname);
+                    friend.put("DescribeYourFriend",getDesc);
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST,
+                        apiurl,
+                        friend,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "succesfully added", Toast.LENGTH_LONG).show();
+
+                            }
+
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), "something went wrong", Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+
+                            RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(jsonObjectRequest);
+
+
+            }
+
         });
+
+
+
+
     }
 }
+
+
+
+
+
+                
+                
